c8ffa3524dbdaf31ff90bb125ef1eaac79e3e803
Nejla Skelic <nskelic1@etf.unsa.ba>
2022-12-05 00:54:15 +0100

    home component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { HomeComponent } from './screens/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/screens/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+type ContentMode = 'movies' | 'shows';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass'],
+})
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {}
+}

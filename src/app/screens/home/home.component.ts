0c104b22423748563991f5489dbd365974588eb2
Nejla Skelic <nskelic1@etf.unsa.ba>
2022-12-06 19:07:59 +0100

    correct tab selection preserved

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BehaviorSubject, combineLatest, map, merge, mergeMap } from 'rxjs';
 import { MoviesService } from 'src/app/services/movies.service';
 import { ShowsService } from 'src/app/services/shows.service';
@@ -16,13 +17,16 @@ export class HomeComponent implements OnInit {
   API_URL = API_URL;
   IMAGE_API_URL = IMAGE_API_URL;
 
-  mode = new BehaviorSubject('shows' as ContentMode);
+  mode = initMode();
   private searchQuery = new BehaviorSubject('');
 
   content = combineLatest([this.mode, this.searchQuery]).pipe(
     mergeMap(([mode, query]) => {
       return query?.length > 2 ? (
         mode === 'movies' ? this.moviesService.searchMovies(query) : this.showsService.searchShows(query)
+        //localStorage.getItem('storedInput') ? (
+        //  mode === 'movies' ? this.moviesService.searchMovies(localStorage.getItem('storedInput')!) : this.showsService.searchShows(localStorage.getItem('storedInput')!)
+        //) : (mode === 'movies' ? this.moviesService.searchMovies(query) : this.showsService.searchShows(query))
       ).pipe(map((data: any) => data?.results))
       : (
         mode === 'movies' ?  this.moviesService.getTopRated() :  this.showsService.getTopRated()
@@ -32,10 +36,12 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private moviesService: MoviesService,
-    private showsService: ShowsService
+    private showsService: ShowsService,
+    private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   setMode = (val: ContentMode) => {
     this.mode.next(val);
@@ -46,5 +52,17 @@ export class HomeComponent implements OnInit {
     this.searchQuery.next(query);
   }
 
+  navigate = (id: any) => {
+    localStorage.setItem('storedMode', this.mode.getValue());
+    localStorage.setItem('storedInput', this.searchQuery.getValue());
+    this.router.navigateByUrl('/article-details/' + this.mode.getValue() + '/' + id);
+  }
+
   debouncedSearch = debounce(this.onSearch, 1000)
 }
+
+function initMode() {
+  return localStorage.getItem('storedMode') ? new BehaviorSubject(localStorage.getItem('storedMode') as ContentMode) : new BehaviorSubject('shows' as ContentMode);
+}
+
+
